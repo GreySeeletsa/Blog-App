57597c499ebd5330fc241301f0c379516ca936a6
GreySeeletsa <seeletsagrey@gmail.com>
2024-03-18 11:57:10 +0200

    Created the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Header from './components/Header';
-import { Footer } from 'flowbite-react';
+import FooterCom from './components/Footer';
 
 export default function App() {
   return (
@@ -20,7 +20,7 @@ export default function App() {
       <Route path="signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
-    <Footer></Footer>
+    <FooterCom/>
     </BrowserRouter>
   )
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,27 +1,10 @@
 import { Footer } from "flowbite-react"
-import { Link } from "react-router-dom"
+//import { Link } from "react-router-dom"
 export default function FooterCom() {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-        <div className="">
+        <div className="w-full max-w-7xl mx-auto">
             <div className="">
-                <div className="">
-                 <Link to="/" className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white">
-                    <span className="px-2 py-1 bg-gradient-to-r from-green-900 via-teal-600 to-slate-800 rounded-lg text-white">Health and Wellness</span>
-                 </Link>
-                </div>
-                <div className="grid grid-col-2 gap-3 sm: mt-4 sm:grid-cols-3 sm:gap-6">
-                    <div>
-                    <Footer.Title title="About"/>
-                    <Footer.LinkGroup col>
-                        <Footer.Link
-                        href='/about'
-                        target="_blank"
-                        rel="noopener noreferrer">
-                            Health and Wellness
-                        </Footer.Link>
-                    </Footer.LinkGroup>
-                    </div>
                     <div>
                         <Footer.Copyright
                         href="#"
@@ -31,7 +14,6 @@ export default function FooterCom() {
                     </div>          
                 </div>
             </div>
-        </div>
     </Footer>
   )
 }
