631e8f44cd9e57482bc29dce479024b1f3d4a836
GreySeeletsa <seeletsagrey@gmail.com>
2024-03-21 14:08:25 +0200

    completed signin page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
             <Button className='w-12 h-10 hidden sm:inline' color='grey' pill>
                 <FaMoon/>
             </Button>
-            <Link to='/sign-in'>
+            <Link to='/signin'>
                 <Button gradientDuoTone='greenToBlue' pill>
                     Sign In
                 </Button>
