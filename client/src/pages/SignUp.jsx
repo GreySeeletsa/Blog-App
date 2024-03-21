3627de5b30a79cf8b796eb057ee2f9e646018aab
GreySeeletsa <seeletsagrey@gmail.com>
2024-03-21 14:09:08 +0200

    completed signin functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -29,7 +29,7 @@ export default function SignUp() {
       }
       setLoading(false);
       if(res.ok) {
-        navigate('/sign-in');
+        navigate('/signin');
       }
     } catch (error) {
       setErrorMessage(error.message);
@@ -74,7 +74,7 @@ export default function SignUp() {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Already have an account?</span>
-            <Link to='/sign-in' className="text-blue-500">
+            <Link to='/signin' className="text-blue-500">
              Sign In            
             </Link>
           </div>
