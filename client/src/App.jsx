7aba6a4108d151aa4f43a09f6cff9606991ef6f8
GreySeeletsa <seeletsagrey@gmail.com>
2024-03-27 13:59:28 +0200

    Added comments section to the admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,6 +28,7 @@ export default function App() {
       <Route element={<OnlyAdminPrivateRoute />}>
       <Route path='/create-post' element={<CreatePost />} />
       <Route path='/update-post/:postId' element={<UpdatePost />} />
+      <Route path='/post/:postSlug' element={<PostPage />} />
       </Route>
       <Route path="/projects" element={<Projects />} />
       <Route path="/post" element={<PostPage />} />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,5 +9,9 @@ export default {
     extend: {},
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("flowbite/plugin"), require('tailwind-scrollbar'),],
+  plugins: [
+    require("flowbite/plugin"),
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/line-clamp'),
+  ],
 };
